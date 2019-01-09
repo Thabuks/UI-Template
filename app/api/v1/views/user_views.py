f31480e9fa6d57548591f94d4d69afa3b64df750
Thabuks <muthabukujohn@gmail.com>
2019-01-09 13:59:58 +0300

    [starts #163069719] creates the login endpoint

---
+++ app/api/v1/views/user_views.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, request, jsonify, make_response
+
+USERVIEW =Blueprint('USERVIEW',__name__)
+
+@USERVIEW.route('/auth/signin', methods =['POST'])
+def landing():
+    """This is the login method"""
+    data = request.get_json()
+    username = data['username']
+    email = data['email']
+
+    return make_response(jsonify({
+        "status": "ok",
+        "username": username,
+        "email": email
+    }), 201)
